254e054f1f42080391b29d888ef9601499ca3486
DevSujal <nimjesujal2004@gmail.com>
2024-06-11 12:10:46 +0530

    "Added @reduxjs/toolkit and react-redux dependencies, updated package-lock.json and package.json, removed App.css file, and modified App.jsx file"

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      
+      <h1 className='text-center bg-black text-xl font-bold text-white p-3 transform translate-y-4'>Todo using redux</h1>
     </>
   )
 }
