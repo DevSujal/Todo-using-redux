e0adcf865efc38c5ba27013c1d85ec1d5da5bbdb
DevSujal <nimjesujal2004@gmail.com>
2024-06-11 12:45:14 +0530

    "Updated App component to include AddTodo and Todo components, and wrapped App in Provider from react-redux in main.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
 
 function App() {
-
   return (
-    <>
-      <h1 className='text-center bg-black text-xl font-bold text-white p-3 transform translate-y-4'>Todo using redux</h1>
-    </>
-  )
+    <div className=" w-3/4 m-auto">
+      <h1 className="text-center bg-black text-xl font-bold text-white p-3 transform translate-y-4">
+        Todo using redux
+      </h1>
+      <div className="flex justify-center">
+        <AddTodo />
+      </div>
+      <Todo />
+    </div>
+  );
 }
 
-export default App
+export default App;
